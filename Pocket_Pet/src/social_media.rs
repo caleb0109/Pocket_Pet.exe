cfb5f98c9192488c4af3e5c0f6253958290750bb
Caleb <caleb0109@outlook.com>
2025-07-24 01:18:09 -0400

    comment button works, will update to make it go to different screen

--- Pocket_Pet/src/social_media.rs
+++ Pocket_Pet/src/social_media.rs
@@ -1,10 +1,12 @@
 use turbo::{serde_json::to_string, *};
+use crate::button::button::ActionButton;
 //need to discuss more about tween and sprite usage
 #[turbo::serialize]
 pub struct SocialMedia{
     pub posted: bool,
     pub ypos: Vec<u32>,
     pub posts: Vec<String>,
+    pub comments: Vec<ActionButton>,
     pub triggered: [bool; 3],
     pub cActive: bool,
 }
@@ -15,6 +17,9 @@ impl SocialMedia {
             posted: false,
             ypos: vec![8],
             posts: vec!["sns_posts#intro".to_string()],
+            comments: vec![ActionButton::new("comment", (60,80,100,10), false),
+                           ActionButton::new("comment", (60,150,100,10), false),
+                           ActionButton::new("comment", (60,220,100,10), false)],
             triggered: [false, false, false],
             cActive: false,
         }
@@ -26,6 +31,7 @@ impl SocialMedia {
         sprite!("sns_posts#intro", x = 58, y = self.ypos[0]);
         text!("PIPI: hey guys! your favorite", x = 63, y = 62, font = "FIVEPIXELS", color = 0xfae3deff);
         text!("virtual pet, pipi is here!!!", x = 63, y = 68, font = "FIVEPIXELS", color = 0xfae3deff);
+        self.comments[0].tempDraw();
 
         if hunger == 0 && !self.triggered[0] {
             self.move_post();
@@ -60,6 +66,7 @@ impl SocialMedia {
                         let a = self.ypos.last().unwrap();
                         sprite!("sns_posts#hunger", x = 58, y = *a);
                         sprite!("icon", x = 38, y = *a);
+                        self.comments[1].tempDraw();
                         //log!("{:?}", a);
                     }     
                 }
@@ -68,6 +75,7 @@ impl SocialMedia {
                         let a = self.ypos.last().unwrap();
                         sprite!("sns_posts#clean", x = 58, y = *a);
                         sprite!("icon", x = 38, y = *a);
+                        self.comments[2].tempDraw();
                     }
                 }
                 _ => {
@@ -111,6 +119,35 @@ impl SocialMedia {
                 log!("{:?}", name);
             }
         }
+    }
+
+    pub fn comment_type(&mut self, current: usize) {
+        let mut comment = "".to_string();
+        let keyboard = keyboard::get(); 
+            // Append keyboard input to the buffer
+            for c in keyboard.chars() {
+                match c {
+                    // Clear the buffer when Enter is pressed
+                    '\n' => {
+                        comment.clear();
+                        self.cActive = false;
+                    }
+
+                    // Append all other chars to the buffer
+                    ch => comment.push(ch),
+                }
+            }
+ 
+            if keyboard.escape().just_pressed() {
+                comment.clear();
+                self.cActive = false;
+            }
+            // Remove the last character when backspace is pressed
+            if keyboard.backspace().just_pressed() {
+                comment.pop();
+            }
+            text!("{:?}", comment; x = self.comments[current].hitbox.0, y = self.comments[current].hitbox.1, color = 0x22406eff, font = "FIVEPIXELS");
+            text!("{:?}", current; x = 0, y = 10, color = 0x22406eff);
     }
 
 }
